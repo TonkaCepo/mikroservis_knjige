b2a1e0d79560cb877cd3390f1a9d1e127836df1c
Tonka <tonka.cepo77@gmail.com>
2026-01-26 09:53:06 +0100

    chore: add h2 database and flyway migrations with roles seed

--- src/main/java/com/example/demo/DevLinksPrinter.java
+++ src/main/java/com/example/demo/DevLinksPrinter.java
@@ -1,3 +1,5 @@
+package com.example.demo;
+
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
